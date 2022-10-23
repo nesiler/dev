3f93a2b6e0a9f61a2ee9a8edaa197f618b806828
Enes Diler <dilerenes@gmail.com>
2022-10-23 23:00:35 +0300

    updates

---
+++ c#_projects/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CartCurt
+{
+class Program{
+
+	static void Main(String[] args){
+		Console.WriteLine("asdasd");
+	}
+
+}
+}
--- c#_projects/learn/learn/Program.cs
+++ c#_projects/learn/learn/Program.cs
@@ -1,5 +1,4 @@
-﻿using Internal;
-using System;
+﻿using System;
 
 namespace learn
 {
@@ -10,35 +9,67 @@ namespace learn
             string nasd = "asd";
             Console.WriteLine(nasd);
 
-            double a = 3.14;
-            int b = Convert.ToInt32(a);
-            System.Console.WriteLine(b.GetType());
+            // int x = Convert.ToInt32(Console.ReadLine());
+            // string y = Console.ReadLine();
+            // System.Console.WriteLine(x);
+            // System.Console.WriteLine(y);
 
-            int x = Convert.ToInt32(Console.ReadLine());
-            string y = Console.ReadLine();
-            System.Console.WriteLine(x);
-            System.Console.WriteLine(y);
-
-/*
-Math:
-Pow, Sqrt, Abs, Round, Ceiling, Floor, Max, Min
-*/
+            /*
+            Math:
+            Pow, Sqrt, Abs, Round, Ceiling, Floor, Max, Min
+            */
 
             Random random = new Random();
             int m = random.Next(0, 10);
             double n = random.NextDouble();
 
-/*
-String
-str.ToUpper, ToLower, Replace(x , y),
-Insert(index, 'text'), Length, SubString(start,count)
-*/
+            /*
+            String
+            str.ToUpper, ToLower, Replace(x , y),
+            Insert(index, 'text'), Length, SubString(start,count)
+            */
 
-            if( 2 > 3){
+            if (2 > 1)
+            {
                 System.Console.WriteLine("false");
             }
 
-            String[] xs = {'a', 'b', 'c', 'd', 'e', 'f'};
+            char[] xs = { 'a', 'b', 'c', 'd', 'e', 'f' };
+
+            //params keyword!!!
+
+            System.Console.WriteLine(CheckOut(1, 2, 3, 4, 5, 6.0, 7.0, 8.45, 89.123, 0.1234));
+
+            static double CheckOut(params double[] prices)
+            {
+                double total = 0;
+                foreach (double price in prices)
+                {
+                    total += price;
+                }
+                return total;
+            }
+
+
+            try
+            {
+                System.Console.WriteLine("Enter something: ");
+                int r = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine("You entered: " + r);
+            }
+            catch (FormatException e)
+            {
+                System.Console.WriteLine("Wtf is that you entered");
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("Something went wrong damn!");
+            }
+            finally
+            {
+                System.Console.WriteLine("thanks!");
+            }
+
             Console.ReadKey();
         }
     }
